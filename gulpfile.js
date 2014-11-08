dde9b5fdeada344bf063ebaeb57a23578a388419
Aleksi Kaistinen <aleksi.kaistinen@gmail.com>
2014-11-08 22:57:36 +0200

    build!

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    concat = require('gulp-concat');
 
 gulp.task('default', function () {
 
@@ -7,4 +8,11 @@ gulp.task('default', function () {
 
     gulp.watch('src/**').on('change', livereload.changed);
 
+});
+
+
+gulp.task('build', function () {
+    gulp.src(['src/engine/*.js', 'src/game/*.js'])
+        .pipe(concat('game.min.js'))
+        .pipe(gulp.dest('./'));
 });
